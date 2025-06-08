d95db68dc78eb69439c56ffacd254fbe085f1357
Mirco Macrelli <mail@mircomacrelli.net>
2025-06-08 15:13:38 +0200

    creazione jar eseguibile

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.gradleup.shadow") version "8.3.6"
 }
 
 group = "net.mircomacrelli"
@@ -11,4 +12,18 @@ repositories {
 
 dependencies {
     implementation("net.jthink:jaudiotagger:3.0.1")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "net.mircomacrelli.obsidian.musica.AlbumGenerator"
+    }
+}
+
+tasks.shadowJar {
+    archiveBaseName.set("obsidian-generators")
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
 }
